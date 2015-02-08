e5d0a1229e1c80fd74ec683d67fb033ed8741848
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2015-02-08 18:07:57 +0900

    call frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk

--- device_202k.mk
+++ device_202k.mk
@@ -19,3 +19,5 @@ $(call inherit-product, build/target/product/full.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_202k
 PRODUCT_DEVICE := 202k
+
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
